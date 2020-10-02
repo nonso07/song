63dc4ae5da9446b86ca73c8627de014537031007
South Habakkuk <ugwuoke2891@gmail.com>
2020-10-02 01:37:12 +0100

    initial commit

---
+++ song.kt
@@ -0,0 +1,35 @@
+/**
+*My favorite music is a highlife music
+https://pl.kotl.in/rrwTkpYhJ
+*/
+
+
+fun main() {
+// My favourite music is Your love is kind
+/*
+ * The attribute is as follows
+ * 
+ * "Artist", "Year Released", "Genre", "Duration" "christian song" 
+ * "Artist Nationality" "Position in the Album list"
+ * 
+  */  
+    
+   var artist:String ="Mercy chionwe"
+    val yearReleased: Int = 2018          // This is a fixed value
+    var genre: String = "Afro music"
+    var duration: Float =10.24f            // the duration time is measured in minute
+    var christianSong: Boolean= true       // This value is true if the song is a christain song.
+    var artistNationality: String = "Nigeria"
+    var positionInTheAlbuumList: Int = 1
+    
+    
+    println("Artist:"+ " " + artist )
+    println("Year Released:"+ " " + yearReleased )
+    println("Genre:"+ " " + genre )
+    println("Duration:"+ " " + duration + " "+"minutes." )
+    println("It is a christain song?:"+ " " + artistNationality)
+    println("Position In The Albuum List:" + " " + "Position number"+positionInTheAlbuumList )
+    
+  
+     
+}
